d61d5462163fdc75c8095db04a178e4d2e0fae08
antonio.trupac <sniper.trupac@gmail.com>
2023-02-08 21:30:20 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Notifications } from "./components";
 
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-light-gray">
+    <main className="flex h-screen items-center justify-center bg-light-gray">
       <Notifications className="h-full w-[730px] rounded-2xl bg-white px-4 md:max-h-[840px] md:px-8" />
-    </div>
+    </main>
   );
 }
 
